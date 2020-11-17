bd9985ddeeec195a444e567cd1a9316a1423a5c8
TheyKay <murraykyleb@gmail.com>
2020-11-16 22:15:13 -0700

    got some routes started

---
+++ controller/routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+
+// router.get("/exercise", (req, res) => {
+//   res.redirect("exercise");
+// })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./controller/routes");
 
 var app = express();
 var PORT = process.env.PORT || 3e3;
 
-app.use(express.static("public"));
+app.use(express.static("public", {
+  extensions: ["html"]
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/pain-tracker",
