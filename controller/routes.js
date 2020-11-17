93c1186db0d6e9d3e4742541047a6387f4985860
TheyKay <murraykyleb@gmail.com>
2020-11-16 22:16:14 -0700

    routes continued

--- controller/routes.js
+++ controller/routes.js
@@ -2,8 +2,54 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-// router.get("/exercise", (req, res) => {
-//   res.redirect("exercise");
-// })
+// async getLastWorkout() {
+//   let res;
+//   try {
+//     res = await fetch("/api/workouts");
+//   } catch (err) {
+//     console.log(err)
+//   }
+//   const json = await res.json();
+//   return json[json.length - 1];
+// },
+router.get("/api/workouts", (req, res) => {
+
+});
+
+// async addExercise(data) {
+//   const id = location.search.split("=")[1];
+//   const res = await fetch("/api/workouts/" + id, {
+//     method: "PUT",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(data)
+//   });
+//   const json = await res.json();
+//   return json;
+// },
+router.post("/api/workouts/:id", (req, res) => {
+
+});
+
+// async createWorkout(data = {}) {
+//   const res = await fetch("/api/workouts", {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//     headers: { "Content-Type": "application/json" }
+//   });
+//   const json = await res.json();
+//   return json;
+// },
+router.post("/api/workouts", (req, res) => {
+
+});
+
+// async getWorkoutsInRange() {
+//   const res = await fetch(`/api/workouts/range`);
+//   const json = await res.json();
+//   return json;
+// },
+router.get("/api/workouts/range", (req, res) => {
+
+});
 
 module.exports = router;
